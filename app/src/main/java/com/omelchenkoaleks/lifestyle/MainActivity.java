e6b2f5f0e128e87318cfe513dc994ea983c9f20e
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-20 05:13:18 +0200

    выбираем из Spinner объект item и его содержимое вставляем в TextView ...

--- app/src/main/java/com/omelchenkoaleks/lifestyle/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/lifestyle/MainActivity.java
@@ -3,12 +3,25 @@ package com.omelchenkoaleks.lifestyle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner mChooseLifeStyle;
+    TextView mWatchGoals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mChooseLifeStyle = findViewById(R.id.spinner_id);
+        mWatchGoals = findViewById(R.id.text_view_id);
+    }
+
+    public void onChooseWayOfLifeClick(View view) {
+        String watchGoals = String.valueOf(mChooseLifeStyle.getSelectedItem());
+        mWatchGoals.setText(watchGoals);
     }
 }
