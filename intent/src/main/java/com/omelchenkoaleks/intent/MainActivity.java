8ebf6fd10eb8b6fe2dc54e39a421d5adeefebcb9
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-21 05:55:21 +0200

    передаем через Интент во вторую активити данные для отображения...

--- intent/src/main/java/com/omelchenkoaleks/intent/MainActivity.java
+++ intent/src/main/java/com/omelchenkoaleks/intent/MainActivity.java
@@ -1,18 +1,34 @@
 package com.omelchenkoaleks.intent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_SAVE_TEXT = "message";
+    EditText mSendText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSendText = findViewById(R.id.edit_text_tv);
     }
 
     public void onSaveTextToSecondActivityClick(View view) {
+        String sendText = mSendText.getText().toString().trim();
+        if (sendText.length() > 0) {
+            Intent intent = new Intent(this, SecondActivity.class);
+            intent.putExtra(EXTRA_SAVE_TEXT, sendText);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "enter text", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
--- intent/src/main/java/com/omelchenkoaleks/intent/SecondActivity.java
+++ intent/src/main/java/com/omelchenkoaleks/intent/SecondActivity.java
@@ -2,13 +2,24 @@ package com.omelchenkoaleks.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SecondActivity extends AppCompatActivity {
+    TextView mReceiveText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        mReceiveText = findViewById(R.id.text_tv);
+
+        Intent intent = getIntent();
+        String receiveText = intent.getStringExtra(MainActivity.EXTRA_SAVE_TEXT);
+        mReceiveText.setText(receiveText);
     }
 }
