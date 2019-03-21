8ebee8e7aeab601decf6dbd1d712b1b5622931c1
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-21 05:34:06 +0200

    две Активити и две разметки для примера набраны...

---
+++ intent/src/main/java/com/omelchenkoaleks/intent/MainActivity.java
@@ -0,0 +1,18 @@
+package com.omelchenkoaleks.intent;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onSaveTextToSecondActivityClick(View view) {
+    }
+}
