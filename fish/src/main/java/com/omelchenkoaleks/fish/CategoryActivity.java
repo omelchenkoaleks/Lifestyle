59bff7859cf63ca2eeb2db6882b5474e4375c5fe
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-03-27 20:46:09 +0200

    layout второй активити с ArrayAdapter ...

--- fish/src/main/java/com/omelchenkoaleks/fish/CategoryActivity.java
+++ fish/src/main/java/com/omelchenkoaleks/fish/CategoryActivity.java
@@ -3,6 +3,8 @@ package com.omelchenkoaleks.fish;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CategoryActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class CategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        ArrayAdapter<Fish> listCategoryAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, Fish.fishArray);
+
+        ListView listFish = findViewById(R.id.category_list_lv);
+        listFish.setAdapter(listCategoryAdapter);
     }
 }
--- fish/src/main/java/com/omelchenkoaleks/fish/Fish.java
+++ fish/src/main/java/com/omelchenkoaleks/fish/Fish.java
@@ -1,4 +1,36 @@
 package com.omelchenkoaleks.fish;
 
 public class Fish {
+    private String name;
+    private String description;
+    private int imageResourceId;
+
+    public static final Fish[] fishArray = {
+            new Fish("crucian", "dwells everywhere", R.drawable.crucian),
+            new Fish("carp", "desired fish", R.drawable.carp),
+            new Fish("pike", "favorite fish", R.drawable.pike),
+    };
+
+    public Fish(String name, String description, int imageResourceId) {
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
